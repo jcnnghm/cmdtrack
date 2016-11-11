51d15905e2136c516ebd5fa8bfa5f1c7360c2e2e
Justin Cunningham <justin@bulletprooftiger.com>
2016-11-10 22:01:31 -0800

    Added a post check

--- server/server.go
+++ server/server.go
@@ -69,23 +69,27 @@ var guestbookTemplate = template.Must(template.New("book").Parse(`
 `))
 
 func sign(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	g := Greeting{
-		Content: r.FormValue("content"),
-		Date:    time.Now(),
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method.", 405)
+	} else {
+		c := appengine.NewContext(r)
+		g := Greeting{
+			Content: r.FormValue("content"),
+			Date:    time.Now(),
+		}
+		if u := user.Current(c); u != nil {
+			g.Author = u.String()
+		}
+		// We set the same parent key on every Greeting entity to ensure each Greeting
+		// is in the same entity group. Queries across the single entity group
+		// will be consistent. However, the write rate to a single entity group
+		// should be limited to ~1/second.
+		key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
+		_, err := datastore.Put(c, key, &g)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, "/", http.StatusFound)
 	}
-	if u := user.Current(c); u != nil {
-		g.Author = u.String()
-	}
-	// We set the same parent key on every Greeting entity to ensure each Greeting
-	// is in the same entity group. Queries across the single entity group
-	// will be consistent. However, the write rate to a single entity group
-	// should be limited to ~1/second.
-	key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
-	_, err := datastore.Put(c, key, &g)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/", http.StatusFound)
 }
