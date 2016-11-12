aaff48dd36e2bae075deff08ca03a4c974880642
Justin Cunningham <justin@bulletprooftiger.com>
2016-11-11 23:13:06 -0800

    Updated for newer app engine

--- server/server.go
+++ server/server.go
@@ -8,12 +8,15 @@ import (
 	"fmt"
 	"net/http"
 
+	"golang.org/x/net/context"
+
 	"github.com/gorilla/mux"
 	"github.com/jcnnghm/cmdtrack/cmd"
 
-	"appengine"
-	"appengine/datastore"
-	"appengine/user"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/user"
 )
 
 // Secret holds the application secret key
@@ -22,22 +25,28 @@ type Secret struct {
 }
 
 // fetchSecret gets or creates a new secret in appengine, and returns it
-func fetchSecret(c appengine.Context) (*Secret, error) {
-	secretKey := datastore.NewKey(c, "Secret", "secret", 0, nil)
-	s := new(Secret)
-	if err := datastore.Get(c, secretKey, s); err != nil {
-		b := make([]byte, 16)
-		if _, err := rand.Read(b); err == nil {
-			key := hex.EncodeToString(b)
-			s.SecretKey = key
-			if _, err := datastore.Put(c, secretKey, s); err != nil {
-				return nil, errors.New("Unable to save secret")
+func fetchSecret(dc context.Context) (s *Secret, err error) {
+	err = datastore.RunInTransaction(dc, func(tc context.Context) error {
+		secretKey := datastore.NewKey(tc, "Secret", "secret", 0, nil)
+		s = new(Secret)
+		if err := datastore.Get(tc, secretKey, s); err != nil {
+			if err != datastore.ErrNoSuchEntity {
+				return err
 			}
-		} else {
-			return nil, errors.New("Unable to generate secret")
+			b := make([]byte, 16)
+			if _, err := rand.Read(b); err == nil {
+				key := hex.EncodeToString(b)
+				s.SecretKey = key
+				if _, err := datastore.Put(tc, secretKey, s); err != nil {
+					return errors.New("Unable to save secret")
+				}
+			} else {
+				return errors.New("Unable to generate secret")
+			}
 		}
-	}
-	return s, nil
+		return nil
+	}, &datastore.TransactionOptions{})
+	return
 }
 
 func init() {
@@ -82,7 +91,7 @@ func verifySecret(fn http.HandlerFunc) http.HandlerFunc {
 		}
 
 		if len(r.Header["Secret"]) != 1 || r.Header["Secret"][0] != secret.SecretKey {
-			c.Debugf("Secret key mismatch")
+			log.Debugf(c, "Secret key mismatch")
 			http.Error(w, "Secret key invalid", http.StatusUnauthorized)
 			return
 		}
@@ -96,16 +105,16 @@ func logCommand(w http.ResponseWriter, r *http.Request) {
 
 	if command, err := cmd.NewCommand(r); err != nil || !command.IsValid() {
 		if err != nil {
-			c.Debugf("Failed with error: %v", err.Error())
+			log.Debugf(c, "Failed with error: %v", err.Error())
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		} else {
 			http.Error(w, "Invalid command data", http.StatusInternalServerError)
 		}
 	} else {
-		c.Debugf("Received Command: %#v", command)
+		log.Debugf(c, "Received Command: %#v", command)
 		err := saveCommand(command, c)
 		if err != nil {
-			c.Debugf("Failed with error: %v", err.Error())
+			log.Debugf(c, "Failed with error: %v", err.Error())
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
@@ -121,18 +130,18 @@ func history(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	c.Debugf("Found Log Lines: %v", len(logLines))
+	log.Debugf(c, "Found Log Lines: %v", len(logLines))
 	encoder := json.NewEncoder(w)
 	if err := encoder.Encode(logLines); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func historyKey(c appengine.Context) *datastore.Key {
+func historyKey(c context.Context) *datastore.Key {
 	return datastore.NewKey(c, "HistoryLog", "default_history_log", 0, nil)
 }
 
-func saveCommand(command *cmd.Command, c appengine.Context) error {
+func saveCommand(command *cmd.Command, c context.Context) error {
 	key := datastore.NewIncompleteKey(c, "HistoryLine", historyKey(c))
 	_, err := datastore.Put(c, key, command)
 	return err
